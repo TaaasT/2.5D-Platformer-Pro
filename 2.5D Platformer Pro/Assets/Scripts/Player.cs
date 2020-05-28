cc68a6c65325580bebd5b27e47eddd5c753210b2
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-05-28 11:15:48 +0200

    Player Movement Controller

---
+++ 2.5D Platformer Pro/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    
+    void Update()
+    {
+        float horizontalInPut = Input.GetAxis("Horizontal");
+
+        Vector3 direction = new Vector3(horizontalInPut, 0, 0);
+
+        _controller.Move(direction * Time.deltaTime);
+
+    }
+}
